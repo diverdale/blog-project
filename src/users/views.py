5d2746729b283045d6a1f715e9822c461b40404c
dalwrigh <dalwrigh@cisco.com>
2020-05-27 11:31:21 -0400

    added Heroku support (gunicorn)

--- src/users/views.py
+++ src/users/views.py
@@ -23,7 +23,7 @@ def login():
             next = request.args.get('next')
 
             if next == None or not next[0] == '/':
-                next = url_for('index')
+                next = url_for('blog.list')
 
             return redirect(next)
     return render_template('login.html', form=form)
